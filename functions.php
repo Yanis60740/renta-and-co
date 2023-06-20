fbc315949d361517721475e5d7dabddabffeaaab
Yanis <94384207+Yanis60740@users.noreply.github.com>
2023-06-20 17:48:56 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -2,8 +2,8 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 try {
-     $db = new PDO('mysql:host=bb1014169-001.eu.clouddb.ovh.net;port=35674;dbname=rentandco', 'admin', 'R3ntAandC02023');
-    // $db = new PDO('mysql:host=localhost;dbname=rentandco', 'root', '');
+     $db = new PDO('', '', '');
+    // $db = new PDO('', '', '');
     // Définir le mode d'erreur PDO sur Exception
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
